b6134a4bd24dbd31f489dc4529f851141560ea9c
JoorDex <diazcoloradojorge@gmail.com>
2025-04-25 14:52:34 -0600

    Se agrega el proyecto final V.1

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+define('ADMIN', 'admin');
+define('RUTA_PRINCIPAL', 'http://localhost/reservas/');
+define('RUTA_ADMIN', RUTA_PRINCIPAL . ADMIN . '/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DATABASE', 'reservas');
+define('CHARSET', 'charset=utf8');
+define('TITLE', 'Hotel Dex');
+//PAYPAL
+define('MONEDA_PAYPAL', 'USD');
+define('CLIENTE_ID', 'ASRT_8YqQhQZEio8BmH9LH3EdfmS5b-jiVjjArXmW-6r92PnoRkhmn5M8sycLTJ8ThA-yHBcTxW306WE');
+//MERCADO PAGO
+define('MONEDA_MP', 'USD');
+define('PUBLIC_KEY', 'TEST-8c3d2f7e-abdc-4732-bae7-6760e9ef5524');
+define('ACCESS_TOKEN', 'TEST-2684339120612399-041523-b35035a7d9725296dccbda68eef7fdf0-1773601694');
+//SMTP
+define('HOST_SMTP', 'smtp.gmail.com');
+define('USER_SMTP', 'lovenaju2@gmail.com');
+define('CLAVE_SMTP', 'dqfpxqgoqrwoxrah');
+define('PUERTO_SMTP', 465);
+
+?>
---
+++ views/principal/servicios/index.php
@@ -0,0 +1,161 @@
+<?php include_once 'views/template/header-principal.php';
+include_once 'views/template/portada.php'; ?>
+
+
+<!-- Start Exclusive Offers Area -->
+<section class="exclusive-offers-area-four pt-100 pb-70">
+    <div class="container">
+        <div class="section-title">
+            <span>OFERTAS EXCLUSIVAS</span>
+            <h2>Puedes conseguir una oferta exclusiva</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-exclusive-four">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/exclusive/7.jpg" alt="Image">
+                    <div class="exclusive-content">
+                        <span class="title">Up To 30% Off</span>
+                        <h3>Swimming for man</h3>
+                        <span class="review">
+                            4.5
+                            <a href="#">(432 Reviews)</a>
+                        </span>
+                        <p>Swimming doller dolor sit aet odu tur adiing elitse</p>
+                        <ul>
+                            <li>
+                                <i class="bx bx-time"></i>
+                                Duration: 2 Hours
+                            </li>
+                            <li>
+                                <i class="bx bx-target-lock"></i>
+                                18+ years
+                            </li>
+                        </ul>
+                        <a href="book-table.html" class="default-btn">
+                            Book Online
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single-exclusive-four">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/exclusive/8.jpg" alt="Image">
+                    <div class="exclusive-content">
+                        <span class="title">This Month Only</span>
+                        <h3>$5 Breakfast package</h3>
+                        <span class="review">
+                            5.0
+                            <a href="#">(580 Reviews)</a>
+                        </span>
+                        <p>Start $5 doller dolor sit aet odeu tur adiing elitse</p>
+                        <ul>
+                            <li>
+                                <i class="bx bx-time"></i>
+                                Duration: 2 Hours
+                            </li>
+                            <li>
+                                <i class="bx bx-target-lock"></i>
+                                18+ years
+                            </li>
+                        </ul>
+                        <a href="book-table.html" class="default-btn">
+                            Book Online
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 offset-sm-3 offset-lg-0">
+                <div class="single-exclusive-four">
+                    <img src="<?php echo RUTA_PRINCIPAL . 'assets/principal/'; ?>img/exclusive/9.jpg" alt="Image">
+                    <div class="exclusive-content">
+                        <span class="title">30% OFFonly This Week</span>
+                        <h3>Free fitness club for women</h3>
+                        <span class="review">
+                            4.9
+                            <a href="#">(580 Reviews)</a>
+                        </span>
+                        <p>Start $5 doller dolor sit aet odeu tur adiing elitse</p>
+                        <ul>
+                            <li>
+                                <i class="bx bx-time"></i>
+                                Duration: 2 Hours
+                            </li>
+                            <li>
+                                <i class="bx bx-target-lock"></i>
+                                18+ years
+                            </li>
+                        </ul>
+                        <a href="book-table.html" class="default-btn">
+                            Book Online
+                            <i class="flaticon-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Exclusive Offers Area -->
+
+<!-- Start facilities Area -->
+<section class="facilities-area-four">
+    <div class="container">
+        <div class="section-title">
+            <span>facilities</span>
+            <h2>Giving entirely awesome</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <div class="singles-facilities">
+                    <i class="flaticon-speaker"></i>
+                    <h3>Meetings & Special Events</h3>
+                    <p>Morem ipsum dol sitamcectur Risus commodo vivercs.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="singles-facilities">
+                    <i class="flaticon-coffee-cup"></i>
+                    <h3>Welcome Drink</h3>
+                    <p>Morem ipsum dol sitamcectur Risus commodo vivercs.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="singles-facilities">
+                    <i class="flaticon-garage"></i>
+                    <h3>Parking Space</h3>
+                    <p>Morem ipsum dol sitamcectur Risus commodo vivercs.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="singles-facilities">
+                    <i class="flaticon-water"></i>
+                    <h3>Cold & Hot Water</h3>
+                    <p>Morem ipsum dol sitamcectur Risus commodo vivercs.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="singles-facilities">
+                    <i class="flaticon-pickup"></i>
+                    <h3>Pick Up & Drop</h3>
+                    <p>Morem ipsum dol sitamcectur Risus commodo vivercs.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="singles-facilities">
+                    <i class="flaticon-swimming"></i>
+                    <h3>Swimming Pool</h3>
+                    <p>Morem ipsum dol sitamcectur Risus commodo vivercs.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End facilities Area -->
+
+<?php include_once 'views/template/footer-principal.php'; ?>
+
+</body>
+
+</html>
